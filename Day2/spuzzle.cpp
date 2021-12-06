a00fd75b1f9a44961a01f8ac424222f965a36395
Sarah-Keppler <sarah.keppler@epitech.eu>
2021-12-06 12:50:05 +0100

    Day2 - puzzle #2

--- Day2/spuzzle.cpp
+++ Day2/spuzzle.cpp
@@ -7,19 +7,28 @@ int main()
 {
     std::ifstream file{"input.txt"};
     std::string line{};
-    unsigned int y{0}, x{0};
+    unsigned int y{0}, x{0}, aim{0};
     while (std::getline(file, line))
     {
         std::string com{line.substr(0, line.find(" "))};
         int val{std::stoi(line.substr(line.find(" ") + 1))};
         if (!com.compare("forward"))
+        {
             x += val;
+            y += val * aim;
+        }
         else if (!com.compare("down"))
-            y += val;
+        {
+            aim += val;
+        }
         else
-            y -= val;
+        {
+            aim -= val;
+        }
     }
     std::cerr << "y: " << y << std::endl
-              << "x: " << x << std::endl;
+              << "x: " << x << std::endl
+              << "aim: " << aim << std::endl
+              << "---------" << std::endl;
     std::cout << y * x << std::endl;
 }
