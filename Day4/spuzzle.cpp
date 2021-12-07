62b24ecfe6c9ebf32178ea94eab3dfd1d61e739d
Sarah-Keppler <sarah.keppler@epitech.eu>
2021-12-07 14:06:44 +0100

    Day 4 : puzzle #1

---
+++ Day4/spuzzle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::ifstream file{"input.txt"};
+    std::string line{};
+    unsigned int y{0}, x{0}, aim{0};
+    while (std::getline(file, line))
+    {
+    }
+}
