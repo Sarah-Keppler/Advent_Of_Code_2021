7a72deab22b155e3b01ca03e442d30cfdf234c54
Sarah-Keppler <sarah.keppler@epitech.eu>
2022-02-25 12:32:04 +0100

    tmp: urgent push

--- Day9/spuzzle.cpp
+++ Day9/spuzzle.cpp
@@ -2,12 +2,55 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
+#include <functional>
+
+typedef std::pair<unsigned int, unsigned int> Point;
+
+void print_disp(std::vector<std::string> const &disp)
+{
+    for (std::string s : disp)
+        std::cerr << s << std::endl;
+    std::cerr << "-------------------" << std::endl;
+}
+
+bool is_visited(std::vector<Point> const &visited, Point const &point)
+{
+    for (Point vp : visited)
+        if (vp.first == point.first && vp.second == point.second)
+            return true;
+    return false;
+}
+
+//void see_horizon(std::vector<std::string> const &map, std::vector<std::string> &disp, std::vector<Point> &visited, unsigned int const x, unsigned int const y)
+void see_horizon(std::vector<std::string> const &map, std::vector<Point> &visited, unsigned int const x, unsigned int const y)
+{
+    std::vector<Point> points{
+        {x, y - 1}, {x + 1, y}, {x, y + 1}, {x - 1, y}};
+    for (Point point : points)
+    {
+        if (0 > point.first || 0 > point.second || map[0].size() <= point.first || map.size() <= point.second)
+            continue;
+        else if (is_visited(visited, point))
+            continue;
+        else if (map[point.second][point.first] != (map[y][x] + 1) || '9' == map[point.second][point.first])
+            continue;
+        else
+        {
+            //disp[point.second][point.first] = 'x';
+            visited.emplace_back(point);
+            //print_disp(disp);
+            //see_horizon(map, disp, visited, point.first, point.second);
+            see_horizon(map, visited, point.first, point.second);
+        }
+    }
+}
 
 bool is_low_point(std::vector<std::string> const &map, unsigned int const x, unsigned int const y)
 {
-    std::vector<std::pair<unsigned int, unsigned int>> points{
+    std::vector<Point> points{
         {x, y - 1}, {x + 1, y}, {x, y + 1}, {x - 1, y}};
-    for (std::pair<unsigned int, unsigned int> point : points)
+    for (Point point : points)
     {
         if (0 > point.first || 0 > point.second || map[0].size() <= point.first || map.size() <= point.second)
             continue;
@@ -24,14 +67,36 @@ int main()
     std::vector<std::string> inputs{};
     while (std::getline(file, line))
         inputs.push_back(line);
-    unsigned int sum{0};
+    std::map<unsigned int, unsigned int, std::greater<int>> basins{};
+    std::vector<Point> visited{};
     for (unsigned int y = 0, ey = inputs.size(); ey > y; ++y)
     {
         for (unsigned int x = 0, ex = inputs[y].size(); ex > x; ++x)
         {
             if (is_low_point(inputs, x, y))
-                sum += std::stoi(std::string{inputs[y][x]}) + 1;
+            {
+                visited.emplace_back(x, y);
+                see_horizon(inputs, visited, x, y);
+                if (basins.find(visited.size()) == basins.end())
+                    basins.insert({visited.size(), 1});
+                else
+                    basins[visited.size()] += 1;
+            }
         }
     }
-    std::cout << sum << std::endl;
+    unsigned int total{1};
+    unsigned int idx{0};
+    for (std::pair<unsigned int, unsigned int> pair : basins)
+    {
+        for (unsigned int it = 0, et = pair.second; et > it; ++it)
+        {
+            ++idx;
+            total *= pair.first;
+            if (3 <= idx)
+                break;
+        }
+        if (3 <= idx)
+            break;
+    }
+    std::cout << total << std::endl;
 }
